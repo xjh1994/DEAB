32b6f485a8a6653ca066000f9f68d8a912327c21
xjh1994 <977135148@qq.com>
2017-12-13 09:59:49 +0800

    no message

---
+++ env.bat
@@ -0,0 +1,6 @@
+﻿echo=1/*>nul&@cls  
+@echo off  
+
+setx JAVA_HOME "C:\Program Files\Java\jdk1.6.0_45"
+setx PATH "%JAVA_HOME%/bin;%JAVA_HOME%/jre/bin"
+setx CLASSPATH ".;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar;"
